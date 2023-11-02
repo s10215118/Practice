0ce8e842ca2f69fc7b0058eb7728733b480fdc32
s10215118 <s10215118@google.com>
2023-11-02 21:10:58 +0800

    Async Function and Await

--- Practice07-AsyncJavascript/Async_JS_CODE/AsyncFunctions/app.js
+++ Practice07-AsyncJavascript/Async_JS_CODE/AsyncFunctions/app.js
@@ -1,107 +1,318 @@
-// async function hello() {
+// async function hello(){
+
+// }
+// console.log(hello());
+
+// const sing = async ()=>{
+//     // throw new Error('BLA BLA BLA');
+//     throw 'ERROR!!!'
+//     return 'LA LA LA LA'
+// }
+// // console.log(sing());
+// sing()
+//     .then((data)=>{
+//         console.log('PROMISE RESOLVED WITH:', data);
+//     })
+//     .catch((err)=>{
+//         console.log('OH NO,',err);
+//     });
+
+// const login = async (username, password) =>{
+//     if(!username || !password) throw 'Missing Credentials.'
+//     if(password === 'corgifeetarecute') return 'WELCOME!'
+//     throw 'Invalid Password.'
+//}
+// login('asdgfasdfasdfda')
+// .then(msg=>{
+//     console.log('LOGGED IN!');
+//     console.log(msg);
+// })
+// .catch(err=>{
+//     console.log('ERROR!');
+//     console.log(err);
+// });
+// login('asdasda','asdfsafasd')
+// .then(msg=>{
+//     console.log('LOGGED IN!');
+//     console.log(msg);
+// })
+// .catch(err=>{
+//     console.log('ERROR!');
+//     console.log(err);
+// });
+// login('asdasdasafa','corgifeetarecute')
+//     .then(msg=>{
+//         console.log('LOGGED IN!');
+//         console.log(msg);
+//     })
+//     .catch(err=>{
+//         console.log('ERROR!');
+//         console.log(err);
+//     });
+
+// const delayedColorChange = (color, delay)=>{
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             document.body.style.backgroundColor = color;
+//             resolve();
+//         },delay);
+//     });
+// };
+// // delayedColorChange('red',1000)
+// //     .then(()=> delayedColorChange('orange',1000))
+// //     .then(()=> delayedColorChange('yellow',1000))
+// //     .then(()=> delayedColorChange('green',1000))
+// //     .then(()=> delayedColorChange('blue',1000))
+// //     .then(()=> delayedColorChange('purple',1000));
+// async function rainbow(){
+//     await delayedColorChange('red',1000);
+//     await delayedColorChange('orange',1000);
+//     await delayedColorChange('yellow',1000);
+//     await delayedColorChange('green',1000);
+//     await delayedColorChange('blue',1000);
+//     await delayedColorChange('purple',1000);
+//     return 'All DONE!';
+// }
+// // rainbow()
+// //     .then((data)=>(console.log('END OF RAINBOW!',data)));
+// async function printRainbow(){
+//     await rainbow();
+//     console.log('END OF RAINBOW!');
+// }
+// printRainbow();
+
+const fakeRequest = (url)=>{
+    return new Promise((resolve, reject)=>{
+        const delay = Math.floor(Math.random()*4500)+500;
+        setTimeout(()=>{
+            if(delay > 4000){
+                reject('Connection Timeout :(');
+            }else{
+                resolve(`Here is your fake data from ${url}`);
+            }
+        },delay);
+    });
+}
+async function makeTwoRequests(){
+    try{
+        let data1 = await fakeRequest('/page/1');
+        console.log(data1);
+        let data2 = await fakeRequest('/page/2');
+        console.log(data2);
+    }
+    catch(e){
+        console.log('CAUGHT THE ERROR!');
+        console.log('THE ERROR IS:',e);
+    }
+    console.log("REQUEST DONE");
+}
+makeTwoRequests();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // async function hello() {
+// // }
+
+
+// const sing = async () => {
+//     throw "OH NO, PROBLEM!"
+//     return 'LA LA LA LA'
+// }
+
+// sing()
+//     .then(data => {
+//         console.log("PROMISE RESOLVED WITH:", data)
+//     })
+//     .catch(err => {
+//         console.log("OH NO, PROMISE REJECTED!")
+//         console.log(err)
+//     })
+
+
+
+
+// const login = async (username, password) => {
+//     if (!username || !password) throw 'Missing Credentials'
+//     if (password === 'corgifeetarecute') return 'WELCOME!'
+//     throw 'Invalid Password'
+// }
+
+// login('todd', 'corgifeetarecute')
+//     .then(msg => {
+//         console.log("LOGGED IN!")
+//         console.log(msg)
+//     })
+//     .catch(err => {
+//         console.log("ERROR!")
+//         console.log(err)
+//     })
+
+
+
+
+
+
+// const delayedColorChange = (color, delay) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = color;
+//             resolve();
+//         }, delay)
+//     })
+// }
+
+// // delayedColorChange('red', 1000)
+// //     .then(() => delayedColorChange('orange', 1000))
+// //     .then(() => delayedColorChange('yellow', 1000))
+// //     .then(() => delayedColorChange('green', 1000))
+// //     .then(() => delayedColorChange('blue', 1000))
+// //     .then(() => delayedColorChange('indigo', 1000))
+// //     .then(() => delayedColorChange('violet', 1000))
+
+
+// async function rainbow() {
+//     await delayedColorChange('red', 1000)
+//     await delayedColorChange('orange', 1000)
+//     await delayedColorChange('yellow', 1000)
+//     await delayedColorChange('green', 1000)
+//     await delayedColorChange('blue', 1000)
+//     await delayedColorChange('indigo', 1000)
+//     await delayedColorChange('violet', 1000)
+//     return "ALL DONE!"
+// }
+
+// // rainbow().then(() => console.log("END OF RAINBOW!"))
+
+
+// async function printRainbow() {
+//     await rainbow();
+//     console.log("END OF RAINBOW!")
+// }
+
+// printRainbow();
+
+// const fakeRequest = (url) => {
+//     return new Promise((resolve, reject) => {
+//         const delay = Math.floor(Math.random() * (4500)) + 500;
+//         setTimeout(() => {
+//             if (delay > 2000) {
+//                 reject('Connection Timeout :(')
+//             } else {
+//                 resolve(`Here is your fake data from ${url}`)
+//             }
+//         }, delay)
+//     })
 // }
 
 
-const sing = async () => {
-    throw "OH NO, PROBLEM!"
-    return 'LA LA LA LA'
-}
+// async function makeTwoRequests() {
+//     try {
+//         let data1 = await fakeRequest('/page1');
+//         console.log(data1);
+//         let data2 = await fakeRequest('/page2');
+//         console.log(data2);
+//     } catch (e) {
+//         console.log("CAUGHT AN ERROR!")
+//         console.log("error is:", e)
+//     }
 
-sing()
-    .then(data => {
-        console.log("PROMISE RESOLVED WITH:", data)
-    })
-    .catch(err => {
-        console.log("OH NO, PROMISE REJECTED!")
-        console.log(err)
-    })
-
-
-
-
-const login = async (username, password) => {
-    if (!username || !password) throw 'Missing Credentials'
-    if (password === 'corgifeetarecute') return 'WELCOME!'
-    throw 'Invalid Password'
-}
-
-login('todd', 'corgifeetarecute')
-    .then(msg => {
-        console.log("LOGGED IN!")
-        console.log(msg)
-    })
-    .catch(err => {
-        console.log("ERROR!")
-        console.log(err)
-    })
-
-
-
-
-
-
-const delayedColorChange = (color, delay) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            document.body.style.backgroundColor = color;
-            resolve();
-        }, delay)
-    })
-}
-
-// delayedColorChange('red', 1000)
-//     .then(() => delayedColorChange('orange', 1000))
-//     .then(() => delayedColorChange('yellow', 1000))
-//     .then(() => delayedColorChange('green', 1000))
-//     .then(() => delayedColorChange('blue', 1000))
-//     .then(() => delayedColorChange('indigo', 1000))
-//     .then(() => delayedColorChange('violet', 1000))
-
-
-async function rainbow() {
-    await delayedColorChange('red', 1000)
-    await delayedColorChange('orange', 1000)
-    await delayedColorChange('yellow', 1000)
-    await delayedColorChange('green', 1000)
-    await delayedColorChange('blue', 1000)
-    await delayedColorChange('indigo', 1000)
-    await delayedColorChange('violet', 1000)
-    return "ALL DONE!"
-}
-
-// rainbow().then(() => console.log("END OF RAINBOW!"))
-
-
-async function printRainbow() {
-    await rainbow();
-    console.log("END OF RAINBOW!")
-}
-
-printRainbow();
-
-const fakeRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        const delay = Math.floor(Math.random() * (4500)) + 500;
-        setTimeout(() => {
-            if (delay > 2000) {
-                reject('Connection Timeout :(')
-            } else {
-                resolve(`Here is your fake data from ${url}`)
-            }
-        }, delay)
-    })
-}
-
-
-async function makeTwoRequests() {
-    try {
-        let data1 = await fakeRequest('/page1');
-        console.log(data1);
-        let data2 = await fakeRequest('/page2');
-        console.log(data2);
-    } catch (e) {
-        console.log("CAUGHT AN ERROR!")
-        console.log("error is:", e)
-    }
-
-}
+// }
