3f2e1040e34abfcf65ff0c6b8f0e696adb3f4c97
s10215118 <s10215118@google.com>
2023-11-08 15:14:48 +0800

    Fetch

--- Practice08-AJAX and AJAJ/app.js
+++ Practice08-AJAX and AJAJ/app.js
@@ -5,16 +5,60 @@
 // console.log(parseData.ticker);
 // console.log(parseData.timestamp);
 
-const req = new XMLHttpRequest();
-req.onload = function(){
-    console.log("Success!!!");
-    // console.log(this);
-    const data = JSON.parse(this.responseText);
-    console.log(data.name, data.hair_color);
+// const req = new XMLHttpRequest();
+// req.onload = function(){
+//     console.log("Success!!!");
+//     // console.log(this);
+//     const data = JSON.parse(this.responseText);
+//     console.log(data.name, data.hair_color);
+// };
+// req.onerror = function(){
+//     console.log("ERROR!!!");
+//     console.log(this);
+// };
+// req.open("GET","https://swapi.dev/api/people/1/");
+// req.send();
+
+// fetch("https://swapi.dev/api/people/1/")
+//     .then(res =>{
+//         console.log("RESOLVED!", res);
+//         return res.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(e =>{
+//         console.log("ERROR!",e);
+//     })
+// fetch("https://swapi.dev/api/people/1/")
+//     .then(res =>{
+//         console.log("RESOLVED!", res);
+//         return res.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//         return fetch("https://swapi.dev/api/people/2/");
+//     })
+//     .then(res =>{
+//         console.log("SECOND REQUEST RESOLVED!!!");
+//         return res.json();
+//     })
+//     .then(data =>{
+//         console.log(data);
+//     })
+//     .catch(e =>{
+//         console.log("ERROR!",e);
+//     })
+const loadStarWarsPeople = async()=>{
+    try{
+        const res = await fetch("https://swapi.dev/api/people/1/");
+        const data = await res.json();
+        console.log(data);
+        const res2 = await fetch("https://swapi.dev/api/people/2/");
+        const data2 = await res2.json();
+        console.log(data2);
+    }catch(e){
+        console.log("ERROR!!!",e);
+    }
 };
-req.onerror = function(){
-    console.log("ERROR!!!");
-    console.log(this);
-};
-req.open("GET","https://swapi.dev/api/people/1/");
-req.send();
+loadStarWarsPeople();
